ec200f6b7da776fdd7d081f6220175fd3b9fa9a9
MICHELLE ZHANG <mzhang8@stanford.edu>
2016-01-26 23:00:35 -0800

    Added a twilio response

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Twilio!"
+    resp = twilio.twiml.Response()
+	resp.say("Hello")
+
+	return str(resp)
 
 if __name__ == '__main__':
     app.run()
