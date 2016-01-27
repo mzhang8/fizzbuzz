9368a2dbdac9b2e48e25a92ac7c9ce45106f0783
MICHELLE ZHANG <mzhang8@stanford.edu>
2016-01-27 01:01:59 -0800

    ed

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def hello():
 
 	# Listen for caller to press keys for number
 
-	g = twilio.twiml.Gather(action="/handle-key", method="GET", finishOnKey="*")
+	with resp.gather(action="/handle-key", method="GET", finishOnKey="*") as g:
+		g.say("GO")
 
 	resp.say("OK. Here we go.")
 
