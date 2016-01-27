5ec9864fbe682428f55941d724fba0df3ee92a41
MICHELLE ZHANG <mzhang8@stanford.edu>
2016-01-26 23:40:58 -0800

    Added response for pressing keys for Twilio

--- app.py
+++ app.py
@@ -6,10 +6,25 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
+	# Greet user
 	resp = twilio.twiml.Response()
-	resp.say("Hello noob")
+	resp.say("Hello noob, enter a number for Phone Buzz.")
+
+	# Listen for caller to press key for number
+	with resp.gather(finishOnKey=*, action="/handle-key", method="POST") as g:
+		g.say("Enter a number and then press star to play Phone Buzz.")
 
 	return str(resp)
+
+@app.route("/handle-key", methods=['GET', 'POST'])
+def handle_key():
+	# Handle number key presses from user
+	digits_pressed = request.values.get('Digits', None)
+	resp = twilio.twiml.Response()
+	resp.say(str(digits_pressed))
+
+	return str(resp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
