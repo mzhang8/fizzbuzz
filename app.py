6f925a53910f3ea9c7b33e8c26ee3f090e4278d6
MICHELLE ZHANG <mzhang8@stanford.edu>
2016-01-26 22:46:28 -0800

    Removed run.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+"""from flask import Flask
 app = Flask(__name__)
 
 
@@ -7,4 +7,21 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run()"""
+
+
+from flask import Flask
+import twilio.twiml
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_fizzbuzz():
+	"""Start the game of Phone Buzz!"""
+	resp = twilio.twiml.Response()
+	resp.say("Hello")
+	
+	return str(resp)
+
+if __name__ == "__main__":
+	app.run(debug=True)
