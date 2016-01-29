2eb79cf75820d60bb0880b3a9aeab0cf1aec5b63
MICHELLE ZHANG <mzhang8@stanford.edu>
2016-01-28 16:53:11 -0800

    Updated with validation that requests are coming from Twilio

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask
 from flask import request
+
+from twilio.util import RequestValidator
 import twilio.twiml
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
-def hello():
+def play():
+	# Validation
+	auth_token = '0eaf817384a273b077e08dcfb187ef13' # input Auth Token here
+
+	validator = RequestValidator(auth_token)
+	url = 'https://phonebuzz-phase1.herokuapp.com/'
+
+
+	params = {    # input parameter values here
+		'CallSid': 'CA26227316e588e817ab2498d42b02e462',
+		'Caller': '+19546517039',
+		'From': '+19546517039',
+		'To': '+17542129667'
+	}
+
+	twilio_signature = request.headers.get('X-Twilio-Signature')
+	print validator.validate(url, params, twilio_signature)
+
 	# Greet user
 	resp = twilio.twiml.Response()
 	resp.say("Hello.")
@@ -15,6 +35,7 @@ def hello():
 		g.say("Enter a number and then wait a few seconds to play Phone Buzz.")
 
 	return str(resp)
+
 
 @app.route("/handle-key", methods=['GET', 'POST'])
 def handle_key():
@@ -34,9 +55,11 @@ def handle_key():
 
 	return str(resp)
 
-def fizzbuzz(n):
 
+def fizzbuzz(n):
 	values = []
+
+	# Store fizz buzz sequence up to n in order
 	for i in range(1, n + 1):
 		if i % 5 == 0 and i % 3 == 0:
 			values.append("fizz buzz")
